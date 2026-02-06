d03a2fc0280510119052d0f9c332cfdd38139f06
mobile-gs2 <xbiaodu@163.com>
2026-02-06 19:36:21 +0800

    update

--- main.js
+++ main.js
@@ -743,15 +743,12 @@ async function main() {
         viewMatrix = JSON.parse(decodeURIComponent(location.hash.slice(1)));
         carousel = false;
     } catch (err) {}
-    // const url = new URL(
-    //     "output.splat",
-    //     location.href,
-    //     // params.get("url") || "train.splat",
-    //     // "https://huggingface.co/cakewalk/splat-data/resolve/main/",
-    // );
-
-    // const url = new URL("output.splat", window.location.href).href;
-    const url = "./output.splat";
+    const url = new URL(
+        // "output.splat",
+        // location.href,
+        params.get("url") || "train.splat",
+        "https://huggingface.co/cakewalk/splat-data/resolve/main/",
+    );
     const req = await fetch(url, {
         mode: "cors", // no-cors, *cors, same-origin
         credentials: "omit", // include, *same-origin, omit
